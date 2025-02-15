f05d5ab922963cc1bde6f1ca3db45ad7a7be26c5
Samuel Wall <oss@samuelwall.co.uk>
2025-02-15 12:39:45 +0000

    refactor: tidy sorting

--- src/utils/sortConfigKeys.ts
+++ src/utils/sortConfigKeys.ts
@@ -21,26 +21,21 @@ const defaultOrder = [
   'dictionaryDefinitions',
   'dictionaries',
   'userWords',
-].reverse()
+]
 
 export function sortConfigKeys(a: string, b: string): number {
   const indexA = defaultOrder.indexOf(a) + 1
   const indexB = defaultOrder.indexOf(b) + 1
 
-  let result = Math.max(Math.min(indexB - indexA, 1), -1)
-
   // Sort alphabetically if both are not found
   if (indexA === indexB) {
-    console.log(indexA, a)
     return a.localeCompare(b)
-    // Sort by order if both are found
   } else if (indexA === 0) {
-    return 1
-  } else if (indexB === 0) {
     return -1
+  } else if (indexB === 0) {
+    return 1
   }
 
-  console.log({a, indexA, b, indexB, result})
-
-  return result // Sort based on the order array
+  // Sort based on the order array
+  return indexA - indexB
 }
