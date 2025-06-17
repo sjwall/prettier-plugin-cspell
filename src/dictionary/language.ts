c299b881886db46fc863f48bec0747dee60ba78e
Samuel Wall <me@samuelwall.uk>
2025-06-17 18:02:12 +0000

    feat(dictionary): check from install location for config

--- src/dictionary/language.ts
+++ src/dictionary/language.ts
@@ -1,13 +1,27 @@
 import {searchForConfig} from 'cspell-lib'
-import {relative} from 'node:path'
+import {dirname, relative} from 'node:path'
+import {fileURLToPath} from 'node:url'
 import type {SupportLanguage} from 'prettier'
 
-const cspellSettings = await searchForConfig(process.cwd())
-const dictionaryDefinitions =
+const searchPaths = [process.cwd(), dirname(fileURLToPath(import.meta.url))]
+
+let cspellSettings: Awaited<ReturnType<typeof searchForConfig>> | undefined
+for (const item of searchPaths) {
+  cspellSettings = await searchForConfig(item)
+  if (cspellSettings) {
+    break
+  }
+}
+
+const dictionaryDefinitions: string[] =
   cspellSettings?.dictionaryDefinitions
     ?.map((v) => v.file ?? v.path)
     .filter(Boolean)
-    .map((v) => relative(process.cwd(), v!)) ?? []
+    .map((v) =>
+      cspellSettings.source?.filename
+        ? relative(dirname(cspellSettings.source!.filename!), v!)
+        : v!,
+    ) ?? []
 
 export const DictionaryParserName = 'cspell-dictionary-parse'
 
